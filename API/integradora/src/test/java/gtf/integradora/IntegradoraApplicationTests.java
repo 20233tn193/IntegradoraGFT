3f2bf71702a76d94e9aebb7c41182e33727aba36
JuanManuel021 <135541859+JuanManuel021@users.noreply.github.com>
2025-03-18 16:58:47 -0600

    api

---
+++ API/integradora/src/test/java/gtf/integradora/IntegradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package gtf.integradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
